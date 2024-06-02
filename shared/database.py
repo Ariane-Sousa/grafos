8677257a5fa135ee92a947996637b1d1db443206
Ariane-Sousa <ariane.crisousa@gmail.com>
2024-06-02 14:44:40 -0300

    fix: ci

--- shared/database.py
+++ shared/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://spotsat:spotsat@localhost/graph_db"
+SQLALCHEMY_DATABASE_URL = "postgresql://spotsat:spotsat@db/graph_db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL
